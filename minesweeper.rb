45809244354668ca12257ea5455202747299a437
niiccolas <40167702+niiccolas@users.noreply.github.com>
2018-12-19 17:11:25 +0100

    Add a play method that runs the game

--- minesweeper.rb
+++ minesweeper.rb
@@ -7,13 +7,25 @@ class MineSweeper
     @board = Board.new(grid_size, num_mines)
   end
 
-  def render
+  def render_minefield
     @board.minefield.each do |row|
       row.each { |square| print square }
       puts
     end
   end
+
+  def play
+    system('clear')
+    render_minefield
+
+    position_input
+  end
+
+  def game_over
+    puts 'Game over!'.yellow
+    exit
+  end
 end
 
 ms = MineSweeper.new(5)
-ms.render
+ms.play
