4590a7a432e67632e30d395f46b26a664569eb4d
niiccolas <40167702+niiccolas@users.noreply.github.com>
2018-12-19 14:27:05 +0100

    Add initialize method including a random seed for mines

---
+++ board.rb
@@ -0,0 +1,21 @@
+require 'colorize'
+
+class Board
+  attr_reader :minefield
+
+  def initialize(grid_size, num_mines)
+    @minefield = Array.new(grid_size) { Array.new(grid_size) { '*' } }
+    seed_mines(num_mines)
+  end
+
+  def seed_mines(num_mines)
+    num_mines.times do
+      x, y = random_position
+      loop do
+        empty_position?(x, y) ? break : (x, y = random_position)
+      end
+
+      @minefield[x][y] = 'B'.red
+    end
+  end
+end
