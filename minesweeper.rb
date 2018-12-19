80461b144a57e119b5361677797c0f67fb25bb55
niiccolas <40167702+niiccolas@users.noreply.github.com>
2018-12-19 14:47:53 +0100

    Add render method for the minefield

--- minesweeper.rb
+++ minesweeper.rb
@@ -6,4 +6,14 @@ class MineSweeper
   def initialize(grid_size, num_mines = grid_size)
     @board = Board.new(grid_size, num_mines)
   end
+
+  def render
+    @board.minefield.each do |row|
+      row.each { |square| print square }
+      puts
+    end
+  end
 end
+
+ms = MineSweeper.new(5)
+ms.render
