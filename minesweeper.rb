c420f9174f2b5f555688ae54a4c05d59e64393ca
niiccolas <40167702+niiccolas@users.noreply.github.com>
2018-12-19 17:12:34 +0100

    Add methods to process user input

--- minesweeper.rb
+++ minesweeper.rb
@@ -25,6 +25,25 @@ class MineSweeper
     puts 'Game over!'.yellow
     exit
   end
+
+  def position_input
+    puts  "\nPlease enter a position on the board (e.g., '0,2')"
+    print '> '
+
+    until valid_input?(gets.chomp.split(',').map(&:to_i))
+      puts 'Error! Enter two numbers, separated by a comma'.red
+      print '> '
+    end
+  end
+
+  def valid_input?(input)
+    input.length == 2 && input.all? { |el| el.is_a? Integer }
+  end
+
+  def mined?(input)
+    row, col = [input]
+    board[row, col] == 'B'
+  end
 end
 
 ms = MineSweeper.new(5)
